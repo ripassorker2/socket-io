693dd8f8a1ff8e7c22544f3371a395362fd5ed80
ripassorker2 <ripassorkar08@gmail.com>
2024-01-23 09:50:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const io = require("socket.io")(7000, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+        users.push({userId, socketId});
+};
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+    // when user connects
+    socket.on("addUser", (userId) => {
+        if (userId) {
+            addUser(userId, socket.id);
+            io.emit("getUsers", users);
+        }
+    });
+
+    // get messages and send messages
+
+    socket.on("sendMessage", ({senderId, reveiverId, message}) => {
+        const user = getUser(reveiverId);
+        if (user) {
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                message,
+            });
+        }
+    });
+
+    // when user disconnect
+    socket.on("disconnect", () => {
+        removeUser(socket.id);
+        console.log("a user has been removed", socket.id);
+        io.emit("getUsers", users);
+    });
+});
